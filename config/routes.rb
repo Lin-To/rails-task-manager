13167be84127b8ef1e05763ca6fc9fcfbcea487f
Lin To <linxuanto@gmail.com>
2019-07-23 16:36:25 +0800

    basic rails CRUD

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,30 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # CRUD
+  # INDEX (READ ALL Tasks)
+  get 'tasks', to: "tasks#index", as: :tasks
+  # # CREATE
+  get 'tasks/new', to: "tasks#new", as: :new
+  post 'tasks', to: "tasks#create", as: :create
+  # (read one tasks)
+  get 'tasks/:id', to: "tasks#show", as: :task
+
+  # UPDATE
+  # #get the form with the restaurant details
+  get 'tasks/:id/edit', to: "tasks#edit", as: :edit
+  patch 'tasks/:id', to: "tasks#update", as: :update
+  # #submit the form (patch or put)
+
+  # #DESTROY RESTAURANT
+  # # restaurants/56
+  delete 'tasks/:id', to: "tasks#destroy", as: :destroy
+
+  root to: "tasks#index"
+
 end
+
+# As a user, I can list tasks
+# As a user, I can view the details of a task
+# As a user, I can add a new task
+# As a user, I can edit a task (mark as completed / update title & details)
+# As a user, I can remove a task
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,5 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Task.create!(title: "laundry", details: "every week wash laundry")
+Task.create!(title: "clean house", details: "deep clean the house")
+Task.create!(title: "Stella bath", details: "take stella to the groomers")
+
+puts "seeds created!"
